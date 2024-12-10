b3c728deb0fe8fbcec39f0c4915b9532f27c2180
pavan singer reddy <pavanreddy.k@1digitals.com>
2024-12-10 12:21:33 +0530

    testing

---
+++ projects/angular-responsive-carousel-ivy/src/lib/carousel.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CarouselComponent} from './carousel.component';
+
+@NgModule({
+    declarations: [
+        CarouselComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        CarouselComponent
+    ],
+    providers: [
+
+    ],
+    bootstrap: [
+        
+    ]
+})
+export class IvyCarouselModule { }
